1e17eb983c8c7ded6e6ee4b90671a80d3087f56d
Tawny Mathi <tawnymslc@gmail.com>
2023-04-13 14:00:47 -0600

    updated reservation screen to send notification when reserving a campsite

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -10,6 +10,7 @@ import {
 import { Picker } from "@react-native-picker/picker";
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 const ReservationScreen = () => {
     const [campers, setCampers] = useState(1);
@@ -35,7 +36,10 @@ const ReservationScreen = () => {
                 },
                 {
                     text: 'OK',
-                    onPress: () => resetForm()
+                    onPress: () => {
+                        presentLocalNotification(date.toLocaleDateString('en-US'))
+                        resetForm()
+                    }
                 }
             ],
         )
@@ -46,6 +50,34 @@ const ReservationScreen = () => {
         setHikeIn(false);
         setDate(new Date());
         setShowCalendar(false);
+    };
+
+    const presentLocalNotification = async (reservationDate) => {
+        const sendNotification = () => {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true,
+                    shouldPlaySound: true,
+                    shouldSetBadge: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${reservationDate} requested`
+                },
+                trigger: null
+            })
+        };
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
     };
 
     return (
